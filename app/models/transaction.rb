c0cb94d7c35ec488bc7d7ae3bbc0903137a9e80c
yuchan <lumir1031@gmail.com>
2020-07-02 16:46:41 +0800

    task 3 fin

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,13 +1,21 @@
 class Transaction < ApplicationRecord
+  attr_accessor :type
   belongs_to :from_wallet, class_name: "Wallet", foreign_key: :from_wallet_id
   belongs_to :to_wallet, class_name: "Wallet", foreign_key: :to_wallet_id
 
-  def in_transactions(t)
+
+  def out_transations(t)
     self.from_wallet_id == t
   end
 
-  def out_transations(t)
+  def in_transactions(t)
     self.to_wallet_id == t
+  end
+
+  def set_type(wallet_id)
+    puts "in set type"
+    @type = wallet_id == self.to_wallet_id ? "label-incoming" : "label-outgoing"
+    self
   end
 
 end
--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -4,7 +4,18 @@ class Wallet < ApplicationRecord
   #has_many :in_trasactions, class_name: "transaction", foreign_key: "to_wallet_id"
 
   def transactions
-    Transaction.where("from_wallet_id = ? OR to_wallet_id = ?", self.id, self.id).order(:updated_at)
+    Transaction.where("from_wallet_id = ? OR to_wallet_id = ?", self.id, self.id).order(:updated_at).map do |transaction|
+      transaction.set_type(self.id)
+    end
+
+    #do |transaction|
+    #  transaction.type = get_type(transaction.to_wallet_id)
+    #  transaction
+    #end
   end
 
+  private
+  def get_type(to_wallet_id)
+    self.id == to_wallet_id ? "INCOMING" : "OUTGOING"
+  end
 end
