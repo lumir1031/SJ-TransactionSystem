02bdf5f98051e65ac6f03bf5fc651b68b32cc5ab
yuchan <lumir1031@gmail.com>
2020-07-05 03:03:55 +0800

    test(model): init a wallet for a new account

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,9 +2,14 @@ class Account < ApplicationRecord
   has_one :wallet
 
   before_create :generate_token
+  after_create :init_wallet
 
   def generate_token
     self.token = SecureRandom.alphanumeric(10)
+  end
+
+  def init_wallet
+    create_wallet(balance: 0)
   end
 
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -2,9 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Account, type: :model do
   context "create account" do
+    let(:account){ create(:account) }
+
     it "generate a token while creating an account" do
-      account = create(:account)
       expect(account.token).not_to be_empty
+    end
+    it "create an empty wallet for a new account" do
+      expect(account.wallet.balance).to be 0
     end
   end
 end
